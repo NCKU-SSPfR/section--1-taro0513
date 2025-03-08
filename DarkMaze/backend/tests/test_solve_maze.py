f5a57143ccb951a825293219d7c8728f3120366b
Patrickuuuu <hank93513@gmail.com>
2025-03-08 16:56:48 +0800

    fix: remove unused variable

--- DarkMaze/backend/tests/test_solve_maze.py
+++ DarkMaze/backend/tests/test_solve_maze.py
@@ -42,12 +42,9 @@ async def move_request(dir):
 @pytest.mark.asyncio
 async def test_integration():
     await login_request()
-    #print(game_state)
     await reset_request()
-    #print(game_state)
-    for i in range(5):
+    for _ in range(5):
         await move_request("down")
-        #print(game_state)
     assert game_state["current_position"] == [1,5]
 
 @pytest.mark.asyncio
